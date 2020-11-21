788a305e512490ff2063068c65827e89fa1e0ea0
LeoCal4 <linkindayd@gmail.com>
2020-11-21 01:57:07 +0100

    Fixed restaurant availability serialization

--- restaurants/models/restaurant_availability.py
+++ restaurants/models/restaurant_availability.py
@@ -18,7 +18,10 @@ class RestaurantAvailability(db.Model):
     restaurant = relationship("Restaurant", back_populates="availabilities")
 
     def serialize(self):
-        return dict([(k,str(v)) for k,v in self.__dict__.items() if k[0] != '_'])
+        att_dict = dict([(k, v) for k,v in self.__dict__.items() if k[0] != '_'])
+        att_dict['start_time'] = str(self.start_time)
+        att_dict['end_time'] = str(self.end_time)
+        return att_dict
 
     def __init__(self, restaurant_id, day, start_time, end_time):
         self.restaurant_id = restaurant_id
